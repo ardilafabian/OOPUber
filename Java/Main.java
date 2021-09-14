01973f3b29fcfc016a191f15c8ca2cac4f426cbc
Fabián Ardila <fabianra97@gmail.com>
2021-09-14 08:40:45 -0500

    :sparkles: Create Java objects

--- Java/Main.java
+++ Java/Main.java
@@ -2,5 +2,10 @@ package Java;
 class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        Car car = new Car();
+        car.licence = "AMQ123";
+        car.driver = "Andrés herrera";
+        car.passenger = 4;
+        car.printDataCar();
     }
 }
