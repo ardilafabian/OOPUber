169267ba8023beb35c74c920baba7b40a812e573
Fabián Ardila <fabianra97@gmail.com>
2021-09-14 09:03:33 -0500

    :sparkles: Create constructor methods on JavaScript

--- JS/Car.js
+++ JS/Car.js
@@ -1,8 +1,12 @@
-function Car() {
+function Car(license, driver) {
     this.id;
-    this.licence;
-    this.driver;
+    this.licence = license;
+    this.driver = driver;
     this.passenger;
+}
 
-    
+Car.prototype.printDatacar = function () {
+    console.log(this.driver);
+    console.log(this.driver.name);
+    console.log(this.driver.document);
 }
---
+++ JS/index.js
@@ -0,0 +1,3 @@
+var car = new Car("AWD123", new Account("Andrés Herrera", "1234567"));
+car.passenger = 5;
+car.printDatacar();
