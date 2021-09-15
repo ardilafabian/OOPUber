b76e7a66c0395ac10c81cbc1c4608df1d0dcc8c8
Fabián Ardila <fabianra97@gmail.com>
2021-09-15 08:47:03 -0500

    :sparkles: use classes to instantiate JS objects

--- JS/Car.js
+++ JS/Car.js
@@ -1,12 +1,14 @@
-function Car(license, driver) {
-    this.id;
-    this.licence = license;
-    this.driver = driver;
-    this.passenger;
-}
+class Car {
+    constructor(license, driver) {
+        this.id;
+        this.licence = license;
+        this.driver = driver;
+        this.passenger;
+    }
 
-Car.prototype.printDatacar = function () {
-    console.log(this.driver);
-    console.log(this.driver.name);
-    console.log(this.driver.document);
+    printDatacar() {
+        console.log(this.driver);
+        console.log(this.driver.name);
+        console.log(this.driver.document);    
+    }
 }
