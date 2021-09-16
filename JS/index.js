a2c620d9d05ebd17f8777ab4cbb9afeac99467f1
Fabián Ardila <fabianra97@gmail.com>
2021-09-16 08:44:18 -0500

    :sparkles: Add inheritance to JS classes

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,7 @@
 var car = new Car("AWD123", new Account("Andrés Herrera", "1234567"));
 car.passenger = 5;
 car.printDatacar();
+
+var car = new UberX("AWD123", new Account("Andrea Herrera", "1234567"));
+car.passenger = 5;
+car.printDatacar();
